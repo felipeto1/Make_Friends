ca086adf6d318005cdf504be8f26178019806dc4
felipeto1 <felipeto1@al.insper.edu.br>
2015-05-04 15:13:08 -0300

    Perguntas para esse trabalho

---
+++ Make_Friends.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:03:09 2015
+
+@author: Felipe
+"""
+"""
+Projeto final Design de Software
+1)	O nome ainda não foi definido mas será algo como MakeFriends.
+
+2)	O grupo é composto por Felipe Telles, Larissa Floriano, Mateus Caruso e Pedro Leon.
+
+3)	O potencial usuário do projeto é alguém que busca conhecer pessoas com gostos parecidos ao seu mas que devido ao isolamento interpessoal da sociedade atual, não consegue.
+
+4)	O programa será feito para alguém que busca conhecer pessoas que tenham gostos parecidos. Para tal, o usuário responderá uma série de perguntas que visam determinar um perfil para essa pessoa. Baseado nas respostas, o programa irá pesquisar em algo que pode ser um banco de dados de usuários e indicar pessoas que combinam com ele. Após o processo ser concluído, os dados fornecidos pelo usuário serão armazenados a fim de que um novo perfil seja criado no banco de dados e que agora um novo utilizador do programa possa ter seu perfil que combine com o de alguém que já utilizou o programa.
+
+5)	Existem alguns produtos parecidos, mas nenhum com a mesma proposta. Exemplo disso é qualquer site de relacionamento que junta pessoas pelo gosto ou até mesmo o famoso aplicativo Tinder que tentava formar casais com base em fotos.
+
+6)	Vídeos tutorias do youtube para caso apareça alguma dúvida, livro Think Python, um maior aprofundamento de classes e objetos pode ser útil e também pesquisas referentes a como criar um banco de dados ou algo parecido.
+
+7)	Caso ocorra algum problema sério que inviabilize essa ideia, pensamos também em outros dois possíveis programas. O primeiro trata de sincronizar o calendário das atividades do Blackboard com um calendário do aluno visando o ajudar a se programar e a se organizar quanto ás suas tarefas. Já o segundo, se baseia em usar dados fornecidos pelo usuário quanto a utilização de seu carro e baseado nisso dar dicas de como melhorar o desempenho do carro.
+
+"""
